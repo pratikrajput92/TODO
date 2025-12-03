2ae76506436eb3d437c59845d33c1114300aa334
Pratik <tpratik083@gmail.com>
2025-12-03 16:20:17 +0530

    connected backend and updated

--- src/component/ADDTask.jsx
+++ src/component/ADDTask.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import AddTask from '../../pages/AddTask';
+import AddTask from '../pages/AddTask';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
